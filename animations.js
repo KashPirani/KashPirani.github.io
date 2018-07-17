60c7a5733889b7dd4ef3a11e3ebacb2e764cf9f0
Kash Pirani <kash@pirani.net>
2018-07-16 23:11:15 -0400

    Added Buttons
    -Animation added to buttons
    -Cleaned up css

---
+++ animations.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {buttonFader();});
+
+function buttonFader(){
+    var delayBoi = 400;
+    var i;
+    var firstElem = 1;
+    var lastElem = 4;
+    for (i = firstElem; i <= lastElem; i ++){
+        var selectorBoi1 = "div button:nth-of-type\(" + i + "\)";
+
+        console.log(selectorBoi1);
+        
+        $(selectorBoi1).delay(delayBoi).fadeIn(400, function(){});
+        delayBoi += 50;
+    }
+}
